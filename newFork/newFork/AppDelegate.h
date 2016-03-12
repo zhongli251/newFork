3f1ca7b24745a86c9ecf8b8581e868f6baba3f5d
zhongli <zhongli@163.com>
2016-03-12 11:32:23 +0800

    项目经理初始化项目!

---
+++ newFork/newFork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newFork
+//
+//  Created by xmg on 16/3/12.
+//  Copyright © 2016年 zl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ newFork/newFork/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  newFork
+//
+//  Created by xmg on 16/3/12.
+//  Copyright © 2016年 zl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSLog(@"项目经理初始化项目");
+}
+
+@end
